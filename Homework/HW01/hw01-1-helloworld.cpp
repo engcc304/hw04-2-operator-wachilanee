02b7df172ee6b9c86c03ba2897f82112eacb16b1
wachilanee <wachilanee47@gmail.com>
2023-07-20 13:38:50 +0700

    hw4-2

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -3,3 +3,17 @@
 //TODO: #4 display me "Hello World." to me.
 
 //TODO: #5 Write your private info into Student-Info.txt
+#include <stdio.h>
+int main()
+{
+    int a = 0 ;
+    int b = 0 ;
+    printf( "Please enter value (a) : " ) ;
+    scanf( "%d", a) ;
+    printf("%d",a) ;
+    printf( "Please enter value (b) : " ) ;
+    scanf( "%d", b) ;
+    printf("%d",b) ;
+    printf("a + b = %d",a+b) ;
+    return 0;
+}
