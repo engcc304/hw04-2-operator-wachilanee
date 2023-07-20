0681817da60ff784faf31541e696784b163be8e8
wachilanee <wachilanee47@gmail.com>
2023-07-20 14:07:14 +0700

    11111

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -17,11 +17,11 @@ int main() {
     printf( "a-- = %d\n", a-- ) ;
     printf( "a += 1 %d\n", a +=1  ) ;
     printf( "a += b %d\n", a += +b ) ;
-    printf( "a -=  %d\n", a -= 1 ) ;
+    printf( "a -=1  %d\n", a -= 1 ) ;
     printf( "a -= b %d\n", a -= b ) ;
-    printf( "a *=  %d\n", a *=1  ) ;
+    printf( "a *=1  %d\n", a *=1  ) ;
     printf( "a *= b %d\n", a *= b ) ;
-    printf( "a %=  %d\n", a %=1  ) ;
+    printf( "a %=1  %d\n", a %=1  ) ;
     printf( "a %= b %d\n", a %= b ) ;
     printf( "a && b %d\n", a && b ) ;
     printf( "a || b %d\n", a || b ) ;
