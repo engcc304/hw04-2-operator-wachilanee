169a46294fe9af8b10c6266be7d3b8703ee301b4
wachilanee <wachilanee47@gmail.com>
2023-07-20 14:05:51 +0700

    update line 18

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -15,7 +15,7 @@ int main() {
     printf( "a % b = %d\n", a % b ) ;
     printf( "++a = %d\n", ++a ) ;
     printf( "a-- = %d\n", a-- ) ;
-    printf( "a +=  %d\n", a +=1  ) ;
+    printf( "a += 1 %d\n", a +=1  ) ;
     printf( "a += b %d\n", a += +b ) ;
     printf( "a -=  %d\n", a -= 1 ) ;
     printf( "a -= b %d\n", a -= b ) ;
